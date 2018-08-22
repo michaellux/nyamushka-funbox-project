e63325c66331af020229946bf9cd9865da154ca4
michaellux <michaellux@yandex.ua>
2018-08-22 20:17:41 +0300

    Импортировал device из 'current-device'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductList from './App';
-import cn from 'cn-decorator'
+import cn from 'cn-decorator';
+import device from 'current-device';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render( <ProductList />,document.getElementById('page__root'));
