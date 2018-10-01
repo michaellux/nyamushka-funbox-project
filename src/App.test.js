747e6f1116d5c0e3b4fc074959548d81d1c95775
michaellux <michaellux@yandex.ua>
2018-10-01 18:54:18 +0300

    Установил полифилл для старых браузеров https://dev.to/letsbsocial1/my-first-time-using-react-160-ehf

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+global.requestAnimationFrme = function(callback) {
+  setTimeout(callback, 0);
+};
